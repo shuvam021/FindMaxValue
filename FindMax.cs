b0f815c708586347857d4b3d55af5899f22f9b3a
Shuvam Das <repository.shuvam@gmail.com>
2022-01-24 17:29:19 +0530

    [Shuvam] Added find max value among 3 given int value

---
+++ FindMax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum;
+
+internal class FindMax
+{
+    public static int FindMaxIntValue(int first, int second, int third)
+    {
+        if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+        {
+            return first;
+        }
+        else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+        {
+            return second;
+        }
+        else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+        {
+            return third;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ internal class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Find Maximum Problem using Generics");
+        Console.WriteLine("Max Int Value:");
+        //test 1 :
+        Console.WriteLine(FindMax.FindMaxIntValue(99, 9, 0));
+        //test 2 :
+        Console.WriteLine(FindMax.FindMaxIntValue(0, 99, 9));
+        //test 2 :
+        Console.WriteLine(FindMax.FindMaxIntValue(9, 0, 99));
     }
 }

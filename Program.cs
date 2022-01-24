d44526c00284cd35299e69662574639cea2ef88e
Shuvam Das <repository.shuvam@gmail.com>
2022-01-24 17:25:15 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FindMaximum;
+
+internal class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Find Maximum Problem using Generics");
+    }
+}
